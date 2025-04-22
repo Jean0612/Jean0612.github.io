e46ecd3135444d728899afeaf4de61b4ce2f73fb
Jean0612 <jeanpol.velez@hotmail.com>
2025-04-22 12:17:27 +0200

    22'04

---
+++ png/index.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const spotlight = document.createElement("div");
+    spotlight.classList.add("spotlight-container");
+    document.body.appendChild(spotlight);
+
+    document.addEventListener("mousemove", (e) => {
+        const x = e.clientX;
+        const y = e.clientY;
+        spotlight.style.background = `radial-gradient(circle 700px at ${x}px ${y}px, rgba(255, 255, 255, 0.15), transparent 60%)`;
+    });
+});
+// Detectar si es móvil con una expresión regular sobre el user agent
+const esMovil = /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent);
+
+// Crear y cargar el link al CSS correcto
+const linkEstilo = document.createElement("link");
+linkEstilo.rel = "stylesheet";
+linkEstilo.type = "text/css";
+linkEstilo.href = esMovil ? "estilo-movil.css" : "estilo-pc.css";
+
+// Agregar el <link> al <head>
+document.head.appendChild(linkEstilo);
